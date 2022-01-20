d82d6c5ba3d50bdcaf24e73f571bfed5ff1232ad
Himanshu7781dhoni <70852622+Himanshu7781dhoni@users.noreply.github.com>
2022-01-20 12:30:43 +0530

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,1 +1,27 @@
-//implementation of graph 
+//implementation of graph using Adjacency list
+public class Graph{
+  private LinkedList<Integer> adj[];
+  public Graph(int v){
+    adj = new LinkedList[v];
+    for(int i = 0; i< v; i++){
+      adj[i] = new LinkedList<Integer>();
+    }
+  }
+  public void AddEdge(int source, int destination){
+     adj[source].add(destination);
+     adj[destination].add(source);
+  }
+}
+public static void main(String args[]){
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter the number of vertices and edges");
+  int v = sc.nextInt();
+  int e = sc.nextInt();
+  Graph graph = new Graph();
+  System.out.println("Enter" + e + "edges");
+  for(int i = 0; i<e ; i++){
+    int source = sc.nextInt();
+    int destination = sc.nextInt();
+    Graph.addEdge(source, destination);
+  }
+}
