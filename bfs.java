59b37fb1ed01737be3a8d946797a44d3e5cf5905
Himanshu Kumar <70852622+hkad-102@users.noreply.github.com>
2022-04-21 03:06:54 +0530

    Update bfs.java

--- bfs.java
+++ bfs.java
@@ -28,3 +28,36 @@ public int bfs(int source, int destination){
  }
 
 }
+
+//best sol
+class Solution{
+  public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>>adj){
+    
+    ArrayList<Integer> bfs = new ArrayList<>();
+    boolean vis[] = new boolean[V + 1];
+    
+    for(int i = 1; i <= V; i++){
+      if(vis[i] == false){
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+        vis[i] = true;
+         
+        while(!q.isEmpty()){
+          
+          
+          Integer node = q.poll();
+          bfs.add(node);
+          
+          for(Integer it : adj.get(node)){
+            if(vis[it] == false){
+              q.add(it);
+              vis[it] = true;
+            }
+          }
+        }
+      }
+    }
+    return bfs;
+  }
+}
