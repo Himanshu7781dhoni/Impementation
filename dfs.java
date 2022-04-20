1c104ff4c0169cf0a8f58bf085227a84e1bc4045
Himanshu Kumar <70852622+hkad-102@users.noreply.github.com>
2022-04-21 03:19:33 +0530

    Update dfs.java

--- dfs.java
+++ dfs.java
@@ -1,7 +1,7 @@
 private boolean dfsUtil(int source, int destination, boolean vis[]){
   if(source == destination) return true;
   for(int neighbour : adj[source]){
-    if(!vis[neighbour]){
+    if(vis[neighbour] == false){
       vis[neighbour] = true;
       boolean isConnected = dfsUtil(neighbour, destination, vis);
       if(isConnected) return true;
@@ -25,13 +25,40 @@ public boolean dfs(int source, int destination){
     int cur = st.pop();
     if(cur == destination) return true;
     for(int neighbour : adj[cur]){
-      if(!vis[neighbour]){
+      if(vis[neighbour] == false){
         vis[neighbour] = true;
         st.push(neighbour);
       }
     }
   }
   return false;
+}
+
+
+
+
+class Solution{
+  public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj){
+    ArrayList<Integer> storeDFS = new ArrayList<>();
+    boolean vis[] = new boolean[V + 1];
+    for(int i = 1; i <= V; i++){
+      if(vis[i] == false){
+        dfs(i, vis, adj, storeDFS);
+      }
+    }
+    return storeDFS;
+  }
+  public void dfs(int i, boolean vis[], ArrayList<ArrayList<Integer>>adj, ArrayList<Integer>storeDFS){
+    //no base case
+    storeDFS.add(i);
+    
+    vis[i] = true;
+    for(Integer it : adj.get(i)){
+      if(vis[it] == false){
+        dfs(it, vis, adj, storeDFS);
+      }
+    }
+  }
 }
       
   
